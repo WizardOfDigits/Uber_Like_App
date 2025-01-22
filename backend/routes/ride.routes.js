501adb39fae26752f55a9884f10cbc18fdffc915
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-23 01:59:35 +0545

    feat(routes): add route for creating a ride with validation

---
+++ backend/routes/ride.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { body } from "express-validator";
+import { authUser } from "../middlewares/auth.middleware.js";
+import { createRide } from "../controllers/ride.controller.js";
+
+const router = express.Router();
+router.post(
+  "/create",
+  authUser,
+  body("pickup")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid pickup address"),
+  body("destination")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid destination address"),
+  body("vehicleType")
+    .isString()
+    .isIn(["auto", "car", "motorcycle"])
+    .withMessage("Invalid vehicle type"),
+  createRide,
+);
+
+export default router;
