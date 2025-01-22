5758f0f04358eaf55055a7507bb148f617de6239
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-23 01:56:02 +0545

    feat(model): add Ride model for managing ride data in the database

---
+++ backend/models/ride.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const rideSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  driver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "driver",
+  },
+  pickup: {
+    type: String,
+    required: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+  },
+  fare: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "ACCEPTED", "ONGOING", "COMPLETED", "CANCELLED"],
+    default: "PENDING",
+  },
+  duration: {
+    type: Number,
+  },
+  distance: {
+    type: Number,
+  },
+  paymentId: {
+    type: String,
+  },
+  orderId: {
+    type: String,
+  },
+  signature: {
+    type: String,
+  },
+});
+
+const rideModel = mongoose.model("ride", rideSchema);
+export default rideModel;
