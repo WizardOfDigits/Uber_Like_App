f2b4c4fd38d32785222384db4ddecf6c18b11e79
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-23 01:55:14 +0545

    feat(controller): add createRide controller to handle ride creation requests

---
+++ backend/controllers/ride.controller.js
@@ -0,0 +1,22 @@
+import { createRideService } from "../services/ride.service.js";
+import { validationResult } from "express-validator";
+
+export const createRide = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { pickup, destination, vehicleType } = req.body;
+  try {
+    const ride = await createRideService({
+      user: req.user._id,
+      pickup,
+      destination,
+      vehicleType,
+    });
+    return res.status(201).json({ ride });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
