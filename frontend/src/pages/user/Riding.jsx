ec2fe72bb06190b028604b82dec4e7dad3d1a702
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-22 17:37:49 +0545

    style: Update Link tag in Riding component with Uber logo and new styles

--- frontend/src/pages/user/Riding.jsx
+++ frontend/src/pages/user/Riding.jsx
@@ -3,6 +3,12 @@ import React from "react";
 function Riding() {
   return (
     <div className="h-screen">
+      <Link
+        to="/home"
+        className="fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full"
+      >
+        <i className="text-lg font-medium ri-home-5-line"></i>
+      </Link>
       <div className="h-1/2">
         <img
           className="h-full w-full object-cover"
@@ -17,40 +23,33 @@ function Riding() {
             alt=""
           />
           <div className="text-right">
-            <h2 className="text-lg font-medium capitalize">Testing Driver</h2>
-            <h4 className="text-xl font-semibold -mt-1 -mb-1">ABC-123A</h4>
+            <h2 className="text-lg font-medium capitalize">Testing</h2>
+            <h4 className="text-xl font-semibold -mt-1 -mb-1">ABC-12-A</h4>
             <p className="text-sm text-gray-600">Maruti Suzuki Alto</p>
-            <h1 className="text-lg font-semibold"> 123</h1>
           </div>
         </div>
+
         <div className="flex gap-2 justify-between flex-col items-center">
           <div className="w-full mt-5">
             <div className="flex items-center gap-5 p-3 border-b-2">
               <i className="text-lg ri-map-pin-2-fill"></i>
               <div>
                 <h3 className="text-lg font-medium">562/11-A</h3>
-                <p className="text-sm -mt-1 text-gray-600">
-                  456 Ocean Drive, Sunset Beach, CA 90210
-                </p>
+                <p className="text-sm -mt-1 text-gray-600"></p>
+                123 Sunshine Avenue, Green Valley, NY 10101
               </div>
             </div>
             <div className="flex items-center gap-5 p-3">
               <i className="ri-currency-line"></i>
               <div>
                 <h3 className="text-lg font-medium">NRS.200.00 </h3>
-                <p className="text-sm -mt-1 text-gray-600">Cash on payment</p>
+                <p className="text-sm -mt-1 text-gray-600">Cash Payment</p>
               </div>
             </div>
           </div>
         </div>
-        <button
-          onClick={() => {
-            props.setVehicleFound(true);
-            props.setConfirmRidePanel(false);
-          }}
-          className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg"
-        >
-          Confirm
+        <button className="w-full bg-green-600 text-white font-semibold p-2 rounded-lg">
+          Make a Payment
         </button>
       </div>
     </div>
