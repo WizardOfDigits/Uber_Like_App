ecebf9d0f79d05dbd2ecc3151a65371f1bd4c15d
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-31 02:58:06 +0545

    fix: update fare display based on vehicle type in confirmride.jsx dynamically

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ConfirmRide(props) {
+const ConfirmRide = (props) => {
   return (
     <div>
       <h5
@@ -11,7 +11,8 @@ function ConfirmRide(props) {
       >
         <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
       </h5>
-      <h3 className="text-2xl font-semibold mb-5">Confirm Your Ride</h3>
+      <h3 className="text-2xl font-semibold mb-5">Confirm your Ride</h3>
+
       <div className="flex gap-2 justify-between flex-col items-center">
         <img
           className="h-20"
@@ -23,24 +24,22 @@ function ConfirmRide(props) {
             <i className="ri-map-pin-user-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600">
-                123 Sunshine Avenue, Green Valley, NY 10101
-              </p>
+              <p className="text-sm -mt-1 text-gray-600">{props.pickup}</p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3 border-b-2">
             <i className="text-lg ri-map-pin-2-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600">
-                456 Ocean Drive, Sunset Beach, CA 90210
-              </p>
+              <p className="text-sm -mt-1 text-gray-600">{props.destination}</p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              <h3 className="text-lg font-medium">NRS.200.00</h3>
+              <h3 className="text-lg font-medium">
+                NRS {props.fare[props.vehicleType]}
+              </h3>
               <p className="text-sm -mt-1 text-gray-600">Cash Cash</p>
             </div>
           </div>
@@ -49,7 +48,7 @@ function ConfirmRide(props) {
           onClick={() => {
             props.setVehicleFound(true);
             props.setConfirmRidePanel(false);
-            props.setVehiclePanel(false);
+            props.createRide();
           }}
           className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg"
         >
@@ -58,6 +57,6 @@ function ConfirmRide(props) {
       </div>
     </div>
   );
-}
+};
 
 export default ConfirmRide;
