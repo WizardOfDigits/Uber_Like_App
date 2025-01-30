d75dcff62e469e4dd6579c6f1d57359448272224
WizardOfDigits <saurav.stamang@gmail.com>
2025-01-31 02:26:51 +0545

    feat(ride): add driver notification and fare calculation in ride controller

--- backend/controllers/ride.controller.js
+++ backend/controllers/ride.controller.js
@@ -1,5 +1,11 @@
-import { createRideService } from "../services/ride.service.js";
+import { createRideService, getFareService } from "../services/ride.service.js";
 import { validationResult } from "express-validator";
+import {
+  getDriversInRadius,
+  getAddressCoordinate,
+} from "../services/map.service.js";
+import rideModel from "../models/ride.model.js";
+import { sendMessageToSocketId } from "../socket.js";
 
 export const createRide = async (req, res) => {
   const errors = validationResult(req);
@@ -7,16 +13,50 @@ export const createRide = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { pickup, destination, vehicleType } = req.body;
+  const { userId, pickup, destination, vehicleType } = req.body;
   try {
-    const ride = await createRideService({
-      user: req.user._id,
+    const ride = await createRideService(
+      req.user._id,
       pickup,
       destination,
       vehicleType,
+    );
+
+    res.status(201).json({ ride });
+
+    const pickupCoordinates = await getAddressCoordinate(pickup);
+    console.log(pickupCoordinates);
+    const driversInRadius = await getDriversInRadius(
+      pickupCoordinates.lat,
+      pickupCoordinates.lng,
+      2,
+    );
+
+    const ridewithUser = await rideModel
+      .findOne({ _id: ride._id })
+      .populate("user");
+    driversInRadius.map((driver) => {
+      sendMessageToSocketId(driver.socketId, {
+        event: `new-ride`,
+        data: ridewithUser,
+      });
     });
-    return res.status(201).json({ ride });
   } catch (error) {
     return res.status(400).json({ message: error.message });
+  }
+};
+
+export const getFare = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { pickup, destination } = req.query;
+
+  try {
+    const fare = await getFareService(pickup, destination);
+    return res.status(200).json(fare);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
 };
