21a3a718406a3e8aae0d05d8d671cfdc0c34a159
WizardOfDigits <saurav.stamang@gmail.com>
2025-02-13 17:10:13 +0545

    feat: Add routing and context management in App.jsx and Main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,49 @@
 import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
+import UserLogout from "./pages/UserLogout";
 import UserSignup from "./pages/UserSignup";
 import DriverLogin from "./pages/DriverLogin";
 import DriverSignup from "./pages/DriverSignup";
+import DriverHome from "./pages/DriverHome";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import DriverProtectWrapper from "./pages/DriverProtectWrapper";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/driver-login" element={<DriverLogin />} />
         <Route path="/driver-signup" element={<DriverSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/driver-home"
+          element={
+            <DriverProtectWrapper>
+              <DriverHome />
+            </DriverProtectWrapper>
+          }
+        />
       </Routes>
     </div>
   );
