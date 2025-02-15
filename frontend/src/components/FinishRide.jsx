0a5164fdd2c1571a35e042ae3b12254ec109f1bc
WizardOfDigits <saurav.stamang@gmail.com>
2025-02-15 16:03:58 +0545

    feat: add FinishRide component with end ride functionality and live tracking

--- frontend/src/components/FinishRide.jsx
+++ frontend/src/components/FinishRide.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function FinishRide(props) {
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const FinishRide = (props) => {
+  const navigate = useNavigate();
+
+  async function endRide() {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/api/v1/rides/end-ride`,
+      {
+        rideId: props.ride._id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      },
+    );
+
+    if (response.status === 200) {
+      navigate("/driver-home");
+    }
+  }
   return (
     <div>
       <h5
@@ -19,7 +41,9 @@ function FinishRide(props) {
             src="https://i.pinimg.com/236x/af/26/28/af26280b0ca305be47df0b799ed1b12b.jpg"
             alt=""
           />
-          <h2 className="text-lg font-medium">Harshi Pateliya</h2>
+          <h2 className="text-lg font-medium">
+            {props.ride?.user.fullname.firstname}
+          </h2>
         </div>
         <h5 className="text-lg font-semibold">2.2 KM</h5>
       </div>
@@ -29,34 +53,40 @@ function FinishRide(props) {
             <i className="ri-map-pin-user-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600">Testina address</p>
+              <p className="text-sm -mt-1 text-gray-600">
+                {props.ride?.pickup}
+              </p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3 border-b-2">
             <i className="text-lg ri-map-pin-2-fill"></i>
             <div>
               <h3 className="text-lg font-medium">562/11-A</h3>
-              <p className="text-sm -mt-1 text-gray-600">Testing addres</p>
+              <p className="text-sm -mt-1 text-gray-600">
+                {props.ride?.destination}
+              </p>
             </div>
           </div>
           <div className="flex items-center gap-5 p-3">
             <i className="ri-currency-line"></i>
             <div>
-              <h3 className="text-lg font-medium">NRS.193.20 </h3>
+              <h3 className="text-lg font-medium">₹{props.ride?.fare} </h3>
               <p className="text-sm -mt-1 text-gray-600">Cash Cash</p>
             </div>
           </div>
         </div>
+
         <div className="mt-10 w-full">
-          <Link
-            to="/driver-home"
+          <button
+            onClick={endRide}
             className="w-full mt-5 flex  text-lg justify-center bg-green-600 text-white font-semibold p-3 rounded-lg"
           >
             Finish Ride
-          </Link>
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
+
 export default FinishRide;
